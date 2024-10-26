d6a27a3a8f98581d60d6b2d1f88267c18575a9e8
nay-hey <neha04gaonkar@gmail.com>
2024-10-26 22:37:13 +0530

    Login

--- backend_folder/server.js
+++ backend_folder/server.js
@@ -15,9 +15,11 @@ connectDB();
 
 const medicineRoute =require('./routes/medicinedb');
 const pendingRoute =require('./routes/pendingdb');
+const loginRoute =require('./routes/loginroute');
 
 app.use('/api/medicinedb', medicineRoute);
 app.use('/api/pendingdb', pendingRoute);
+app.use('/api/login', loginRoute);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
